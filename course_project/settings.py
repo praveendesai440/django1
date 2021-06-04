f5c4df8cebfcee3c2f01fd5abbfbe1c7c80aa020
praveendesai440 <47137647+praveendesai440@users.noreply.github.com>
2021-06-04 12:51:16 +0530

    commit

--- course_project/settings.py
+++ course_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-jz4trzlx=rpzcv9hwil*h!bnd5h0!623frj*f)rf3rqhe)wll@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','']
+ALLOWED_HOSTS = ['127.0.0.1','myhomedp.herokuapp.com']
 
 
 # Application definition
